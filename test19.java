56b6d28a6e72f7d3247faedff964c11ac6f94cbf
blast66 <113650618+blast66@users.noreply.github.com>
2022-11-18 11:51:27 +0800

    Add files via upload

---
+++ test19.java
@@ -0,0 +1,52 @@
+public class test19 {
+    public static void main(String[] args) { 
+        for(int i = 1; i<=14; i++){
+			
+            for(int z=1; z<=12;z++){
+                if(i<7){
+                    if(z<7){
+                        if(z<=7-i){
+                            System.out.print("*");
+                        }
+                        else{
+                            System.out.print(" ");	
+                        }
+                        
+                    }
+                    if(z>=6){
+                        if(z<6+i){
+                            System.out.print(" ");	
+                        }
+                        else{
+
+                            System.out.print("*");
+                        }
+                    }
+                    }
+                
+                    if(i>7){
+                        if(z<7  ){
+                            if(z<=i-7){
+                                System.out.print("*");
+                            }
+                            else{
+                                System.out.print(" ");	
+                            }
+                            
+                        }
+                        if(z>=6){
+                            if(z<6+i){
+                                System.out.print("*");	
+                            }
+                            else{
+    
+                                System.out.print(" ");
+                            }
+                        }
+                        }
+            }
+            
+            System.out.println();
+        }
+    }
+}
